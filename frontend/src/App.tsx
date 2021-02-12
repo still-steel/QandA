efdf8b6c459ed8b8087efe423963ade578e3ec07
still-steel <antonabramow22@gmail.com>
2021-02-12 13:24:30 +0300

    Компоненты стилизованы с использованием Emotion.js

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 
+import { fontFamily, fontSize, gray2 } from './Styles';
+
 function App() {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
       <Header />
       <HomePage />
     </div>
